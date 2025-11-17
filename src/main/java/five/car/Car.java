b331804d2e762b32844713bcdb7a46fb11886cb4
Vladislav <52047456+VlaFi@users.noreply.github.com>
2025-11-17 19:02:14 +0400

    Create Car.java

---
+++ src/main/java/five/car/Car.java
@@ -0,0 +1,47 @@
+package five.car;
+
+public class Car {
+    private final String brand;
+    private final int power;
+    private final int year;
+
+    private Car(Builder builder) {
+        this.brand = builder.brand;
+        this.power = builder.power;
+        this.year = builder.year;
+    }
+
+    public String getBrand() { return brand; }
+    public int getPower() { return power; }
+    public int getYear() { return year; }
+
+    @Override
+    public String toString() {
+        return "Автомобиль: бренд = " + brand + ", мощность = " + power + " лс" + ", год = " + year;
+    }
+
+    public static class Builder {
+        private String brand;
+        private int power;
+        private int year;
+
+        public Builder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public Builder power(int power) {
+            this.power = power;
+            return this;
+        }
+
+        public Builder year(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
+}
