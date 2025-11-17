33b2069158313c7805a340c37c28483d65790292
A-S-Shalov <shalov911@gmail.com>
2025-11-18 00:22:04 +0300

    printMenu

--- src/main/java/five/App.java
+++ src/main/java/five/App.java
@@ -1,7 +1,12 @@
 package five;
 
+import five.ui.ConsoleApp;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println("Сейчас используется временная заглушка Car. Удалить перед финальной версией!");
 
+        ConsoleApp consoleApp = new ConsoleApp();
+        consoleApp.run();
     }
 }
--- src/main/java/five/ui/ConsoleApp.java
+++ src/main/java/five/ui/ConsoleApp.java
@@ -1,4 +1,45 @@
 package five.ui;
 
+import java.util.Scanner;
+
 public class ConsoleApp {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    private void printMenu() {
+        System.out.println("\n=== МЕНЮ ===");
+        System.out.println("1. Заполнить массив автомобилей");
+        System.out.println("2. Отсортировать массив");
+        System.out.println("3. Показать текущий массив");
+        System.out.println("0. Выход");
+        System.out.print("Ваш выбор: ");
+    }
+
+    public void run(){
+        boolean running = true;
+
+        while (running){
+            printMenu();
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1" -> {
+                    System.out.println("Тут будет заполнение массива");
+                }
+                case "2" -> {
+                    System.out.println("Тут будет сортировка");
+                }
+                case "3" -> {
+                    System.out.println("Тут будет вывод текущего массива");
+                }
+                case "0" -> {
+                    System.out.println("Выход из программы");
+                    running = false;
+                }
+                default -> System.out.println("Неизвестная команда, попробуйте ещё раз.");
+            }
+        }
+
+
+    }
 }
